7d68c0027ec486f2a374cdd54a243729e23562ad
Tshepo Thoriso Shale <tsheposhale@yahoo.com>
2024-03-04 10:56:01 +0200

    added font family from Google fonts. Changes made in index.css file.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
-     <h1>Basotho Pizza</h1>
+     <Navbar/>
     </div>
   );
 }
