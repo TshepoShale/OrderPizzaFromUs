926ee56e58b5de2c4f77feb07304b42a95e3aa14
Tshepo Thoriso Shale <tsheposhale@yahoo.com>
2024-03-04 11:32:38 +0200

    navbar changed. added shadow effect.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg">
+      <nav className="navbar navbar-expand-lg shadow-lg p-3 mb-5 bg-white rounded">
         <a className="navbar-brand" href="#">
           Navbar
         </a>
