456bb0147fc84d227909c5ec92db371c0d9defec
Tshepo Thoriso Shale <tsheposhale@yahoo.com>
2024-03-04 11:25:57 +0200

    changed navbar item names.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -20,14 +20,14 @@ export default function Navbar() {
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ml-auto">
-            <li className="nav-item active">
+            <li className="nav-item">
               <a className="nav-link" href="#">
-                Home <span className="sr-only">(current)</span>
+                Login
               </a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">
-                Features
+                Cart
               </a>
             </li>
           </ul>
