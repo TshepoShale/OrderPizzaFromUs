655d8580d83c833c12cc995c7fb2e5d3aa19943f
Tshepo Thoriso Shale <tsheposhale@yahoo.com>
2024-03-05 11:40:47 +0200

    fixed decimal points to only two fixed positions.

--- client/src/pizzadata.js
+++ client/src/pizzadata.js
@@ -2,7 +2,7 @@ const pizzas = [
   {
     name: "Pepper BBQ Chicken",
     variants: ["Small", "Large", "X-Large"],
-    prices: { "Small": 29.90, "Large": 84.90, "X-Large": 124.90 },
+    prices: { "Small": 29.90, "Large": 84.90, "X-Large": 125.00 },
     category: "NonVeg",
     image:"https://img.freepik.com/free-photo/chicken-pizza-with-bell-peppers-herbs-barbecue-sauce_140725-8379.jpg?w=740&t=st=1709626647~exp=1709627247~hmac=f2422f7893b1e86ff3f067bf83fb48715007b46b4dfa3430c4b6b0e245a4fe0f",
     description:
@@ -11,7 +11,7 @@ const pizzas = [
   {
     name: "Sausage Pizza",
     variants: ["Small", "Large", "X-Large"],
-    prices: { "Small": 29.90, "Large": 80.00, "X-Large": 115.90 },
+    prices: { "Small": 29.90, "Large": 80.00, "X-Large": 115.00 },
     category: "NonVeg",
     image:
       "https://img.freepik.com/free-photo/top-view-delicious-pizza-with-pepperoni_23-2149235628.jpg?w=900&t=st=1709626224~exp=1709626824~hmac=1e3e0a0e3d51b8c1be21b6024d5f22fa8c0495d9ad4c17eef8ed3005a6ddfeb2",
@@ -29,7 +29,7 @@ const pizzas = [
   {
     name: "Veggie Pizza",
     variants: ["Small", "Large", "X-Large"],
-    prices: { "Small": 29.90, "Large": 75.00, "X-Large": 117.90 },
+    prices: { "Small": 29.90, "Large": 75.00, "X-Large": 117.00 },
     category: "Veg",
     image:
       "https://img.freepik.com/free-photo/top-view-tasty-pizza-with-pepper_23-2148601577.jpg?w=900&t=st=1709626354~exp=1709626954~hmac=a1bddd1f31766664396c1c36fc593ab665f674c2eca878d9eab916020e7d1b1c",
@@ -38,7 +38,7 @@ const pizzas = [
   {
     name: "Jalapeno Pizza",
     variants: ["Small", "Large", "X-Large"],
-    prices: { "Small": 29.90, "Large": 89.00, "X-Large": 114.90 },
+    prices: { "Small": 29.90, "Large": 89.00, "X-Large": 114.00 },
     category: "Veg",
     image:
       "https://c1.wallpaperflare.com/preview/192/114/498/pizza-dough-refreshment-cheese.jpg",
@@ -47,7 +47,7 @@ const pizzas = [
   {
     name: "Hawaiian Pizza",
     variants: ["Small", "Large", "X-Large"],
-    prices: { "Small": 29.90, "Large": 89.00, "X-Large": 117.90 },
+    prices: { "Small": 29.90, "Large": 89.00, "X-Large": 117.00 },
     category: "Veg",
     image:
       "https://img.freepik.com/free-photo/hawaiian-pizza_74190-2501.jpg?w=900&t=st=1709626461~exp=1709627061~hmac=230c8871010149f5913cdc4a455d309f81cedae8e4e09b91406df2b2ca4228eb",
