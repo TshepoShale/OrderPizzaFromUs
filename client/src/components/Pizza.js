13dca11b4a5da2b5c4a16bf9cd3a8b8f616c1634
Tshepo Thoriso Shale <tsheposhale@yahoo.com>
2024-03-05 13:39:56 +0200

    played with the margins and padding. It's better looking now.

--- client/src/components/Pizza.js
+++ client/src/components/Pizza.js
@@ -5,37 +5,36 @@ export default function Pizza({ pizza }) {
     const [variant, setVariant] = useState('Small'); // Default value is set to 'Small'.
 
     return (
-        <div style={{margin:'100px'}}> 
+        <div style={{ margin: '50px' }} className='shadow-lg p-3 mb-5 bg-white rounded'>
             <h1>{pizza.name}</h1>
             <img src={pizza.image} className="img-fluid" style={{ height: '200px', width: '200px' }} alt={pizza.name} />
-            
-            <div className="flex-container"> 
+
+            <div className="flex-container">
                 <div className='w-100 m-1'>
                     <p>Variants:</p>
-                    <select className='form-control' value={variant} onChange={(e) => setVariant(e.target.value)}> 
+                    <select className='form-control' value={variant} onChange={(e) => setVariant(e.target.value)}>
                         {pizza.variants.map((variant, index) => (
                             <option key={index}>{variant}</option>
                         ))}
                     </select>
                 </div>
 
-                <div className='w-100 m-1'> 
+                <div className='w-100 m-1'>
                     <p>Quantity:</p>
-                    <select className='form=control' value={quantity} onChange={(e) => setQuantity(e.target.value)}> 
+                    <select className='form-control' value={quantity} onChange={(e) => setQuantity(e.target.value)}>
                         {[...Array(10).keys()].map((x, i) => (
-                            <option key={i+1} value={i+1}>{i+1}</option>
+                            <option key={i + 1} value={i + 1}>{i + 1}</option>
                         ))}
                     </select>
                 </div>
             </div>
-        
+
             <div className="flex-container">
-                <div className='m-1 w-100'>
+                <div className='w-90 m-1'>
                     <h1>Price: {pizza.prices[variant] * quantity}</h1> {/* Calculation of total. */}
                 </div>
 
-
-                <div>
+                <div className='m-1'>
                     <button className="btn">ADD TO CART</button>
                 </div>
             </div>
