8060148a880463a2a2fd4287ef05b46bdecec8b4
Tshepo Thoriso Shale <tsheposhale@yahoo.com>
2024-03-05 11:07:48 +0200

    added array for quanitity. Ordering from 1 pizza, up to 10, per flavour. Per customer.

--- client/src/components/Pizza.js
+++ client/src/components/Pizza.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export default function Pizza({pizza}) {
+export default function Pizza({ pizza }) {
     return (
         <div>
             <h1>{pizza.name}</h1>
-            <img src={pizza.image} className="img-fluid" style={{height: '200px', width: '200px'}} alt={pizza.name} />
+            <img src={pizza.image} className="img-fluid" style={{ height: '200px', width: '200px' }} alt={pizza.name} />
             
             <div className="flex-container"> 
                 <div className='w-100'>
@@ -17,7 +17,12 @@ export default function Pizza({pizza}) {
                 </div>
 
                 <div className='w-100'> 
-                    <p>Prices:</p>
+                    <p>Quantity:</p>
+                    <select> //here is the quantity section. You can only order 10 pizzas, per flavour. 
+                        {[...Array(10).keys()].map((x, i) => (
+                            <option key={i+1} value={i+1}>{i+1}</option>
+                        ))}
+                    </select>
                 </div>
             </div>
         </div>
