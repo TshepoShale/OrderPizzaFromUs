4d85da9be365664a5857adae8decd7bc6410bc9b
Tshepo Thoriso Shale <tsheposhale@yahoo.com>
2024-03-05 10:46:02 +0200

    added dropdown for pizza sizes.

--- client/src/components/Pizza.js
+++ client/src/components/Pizza.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-export default function Pizza({pizza}){
+export default function Pizza({pizza}) {
     return (
         <div>
             <h1>{pizza.name}</h1>
-            <img src={pizza.image} className="img-fluid" style={{height: '200px', width: '200px'}}/>
+            <img src={pizza.image} className="img-fluid" style={{height: '200px', width: '200px'}} alt={pizza.name} />
+            
+            <div className="flex-container"> 
+                <div className='w-100'>
+                    <p>Variants:</p>
+                    <select>
+                        {pizza.variants.map(variant => (
+                            <option key={variant}>{variant}</option>
+                        ))}
+                    </select>
+                </div>
+
+                <div className='w-100'> 
+                    <p>Prices:</p>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
